c50a34063a65209226762a370ac9b47640b92e03
three-onefouronefive <lawasknowell51@gmail.com>
2026-05-15 17:19:20 +0800

    fix: resolve leaflet marker icon paths

--- frontend/src/components/ContactEnquiryMap.jsx
+++ frontend/src/components/ContactEnquiryMap.jsx
@@ -1,4 +1,18 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: markerIcon2x,
+    iconUrl: markerIcon,
+    shadowUrl: markerShadow,
+});
 
 function ContactEnquiryMap() {
     const position = [13.661293605573427, 121.05115835513249]; 
