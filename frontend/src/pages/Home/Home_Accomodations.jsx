36d872f6a79df279a74bf3c4c84ee0e8ec3556af
three-onefouronefive <lawasknowell51@gmail.com>
2026-05-16 18:59:09 +0800

    fix: added some routing for buttons @ home page

--- frontend/src/pages/Home/Home_Accomodations.jsx
+++ frontend/src/pages/Home/Home_Accomodations.jsx
@@ -1,15 +1,17 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import SuiteGrid from '../../components/SuiteGrid';
 import '../../css/Accomodations.css';
 
 function Home_Accomodations() {
+    const navigate = useNavigate()
+
     return (
         <>
             <div className="accomodations_main_wrapper">
                 <div className="accomodations_subtitle anim-up">ACCOMODATIONS</div>
                 <div className="accomodations_suites anim-up">
                     <h1>Our <span className='italics'>Suites</span></h1>
-                    <Link to="/suites"><div className='accomodations_cta'>VIEW ALL SUITES →</div></Link>
+                    <div className='accomodations_cta' onClick={() => navigate('/suites')}>VIEW ALL SUITES →</div>
                 </div>
                 <div className='anim-up'>
                     <SuiteGrid />
